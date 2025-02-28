c09e7a1ffca70c9bb522ed37ac99de7938667dc7
Karen Chahrour <karenchahrour123@gmail.com>
2025-02-28 06:52:39 -0800

    Creating abstract display method

--- rentals.py
+++ rentals.py
@@ -1,9 +1,16 @@
-class Vehicals:
+from abc import ABC, abstractmethod
+
+class Vehicals(ABC):
     def __init__(self, brand, model, year, rental_price_per_day):
         self.brand = brand
         self.model = model
         self.year = year
         self.rental_price_per_day = rental_price_per_day
+
+     # method to display cars info
+    @abstractmethod
+    def display_info(self):
+        pass
 
 
 
